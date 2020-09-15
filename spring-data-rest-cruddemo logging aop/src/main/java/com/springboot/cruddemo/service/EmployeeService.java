5b1a017025fef7d197cb5081d925e61cdd625255
claudiobrancaccio <BrancaccioCl@valueteam.com>
2020-09-15 09:57:32 +0200

    bho

---
+++ spring-data-rest-cruddemo logging aop/src/main/java/com/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.springboot.cruddemo.service;
+
+import com.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> findAll();
+
+    public Employee findById(int theId);
+
+    public void save(Employee theEmployee);
+
+    public void deleteById(int id);
+}
